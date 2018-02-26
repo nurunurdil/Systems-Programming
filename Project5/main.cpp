423637dc7c948941f1abbb849b631878e2301ed3
Nuru Nurdil <nurun@nubessom.com>
2018-02-26 21:50:01 +0400

    Fix of initial errors

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
+/*/
 #include <dirent.h>
 #include <pwd.h>
+*/
 
-int main(int charv, char[] *argc) {
+
+int main(int charv, char *argc []) {
 	//The following is the main class for the 'LS' project in Systems Programming class
 
 
