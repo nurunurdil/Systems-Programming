91246b2f5300a68992d5ab8466079bdd9f83e92f
Nuru Nurdil <nurun@nubessom.com>
2018-03-08 15:15:13 +0400

    First abstract setup of the structure

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,12 +1,48 @@
+/*
+*@author: Nuru Nurdil
+*/
+
 #include <stdio.h>
-/*/
+#include <sys/stat.h>
+/*
+#include <sys/ioctl.h>
 #include <dirent.h>
 #include <pwd.h>
 */
+char option;
+char *path;
+char *fileName;
 
+//Set the option of the ls command
+static void setOption(char *argv) {
+	option = *argv;
+}
 
-int main(int charv, char *argc []) {
+//Check if the option requires additional fileName
+static int checkFileNameNecessity(char option, int argc) {
+	return 0;
+}
+
+//Print the list
+static void getList(int fileNeeded, char *fileName) {
+
+}
+
+int		main(int argc, char *argv []) {
 	//The following is the main class for the 'LS' project in Systems Programming class
 
+	/*
+	//Checking the number of inputted parameters
+	if (argc < 1) {
+		printf("Not enough parameters for list function\n");
+		//exit(1);
+		return 0;
+	}
+	*/
 
+
+	setOption(argv[1]);
+	int fileNeeded = checkFileNameNecessity(option, argc);
+	getList(fileNeeded, fileName);
+	
 }
