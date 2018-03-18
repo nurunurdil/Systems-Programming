a9830f59aa8bbe2a0a2b32f1765a4df8f8c63506
Nuru Nurdil <nurunurdil@gmail.com>
2018-03-18 14:32:31 +0400

    Setting the option flags

--- List_Project/list.cpp
+++ List_Project/list.cpp
@@ -1,21 +1,67 @@
 /*
 *@author: Nuru Nurdil
 */
-
+#include <unistd.h>
 #include <stdio.h>
 #include <sys/stat.h>
-/*
 #include <sys/ioctl.h>
 #include <dirent.h>
 #include <pwd.h>
-*/
-char option;
+
+short option = 0;
 char *path;
 char *fileName;
+void setOption(char opt);
 
 //Set the option of the ls command
-static void setOption(char *argv) {
-	option = *argv;
+static void setOption(char opt) {
+	switch(opt){
+		//Upper-case letters set the last 3 bits of the short number 'option'
+		case 'A':
+			option = option | (1<<0);
+			break;
+		case 'R':
+			option = option | (1<<1);
+			break;
+		case 'S':
+			option = option | (1<<2);
+			break;
+		
+		//Lower-case letters set the other 11 bits of the short number 'option'
+		case 'a':
+			option = option | (1<<3);
+			break;
+		case 'c':
+			option = option | (1<<4);
+			break;
+		case 'd':
+			option = option | (1<<5);
+			break;
+		case 'f':
+			option = option | (1<<6);
+			break;
+		case 'h':
+			option = option | (1<<7);
+			break;
+		case 'i':
+			option = option | (1<<8);
+			break;
+		case 'l':
+			option = option | (1<<9);
+			break;
+		case 'n':
+			option = option | (1<<10);
+			break;
+		case 'r':
+			option = option | (1<<11);
+			break;
+		case 't':
+			option = option | (1<<12);
+			break;
+		case 'u':
+			option = option | (1<<13);
+			break;	
+	}
 }
 
 //Check if the option requires additional fileName
@@ -29,6 +75,7 @@ static void getList(int fileNeeded, char *fileName) {
 }
 
 int		main(int argc, char *argv []) {
+	int opt;
 	//The following is the main class for the 'LS' project in Systems Programming class
 
 	/*
@@ -40,8 +87,11 @@ int		main(int argc, char *argv []) {
 	}
 	*/
 
+	while((opt = getopt(argc,argv,"AacdFfhilnRrStu")) != -1){
+		setOption(opt);
+	}
 
-	setOption(argv[1]);
+
 	int fileNeeded = checkFileNameNecessity(option, argc);
 	getList(fileNeeded, fileName);
 	
